f8bc2d4dcec61e62272ec3c6754c9759c7399422
Sri Ram <k.sriramsagar@gmail.com>
2018-09-24 15:59:30 -0400

    Removed input.

--- NEUR634_homework/HW-4/neuron_hw.py
+++ NEUR634_homework/HW-4/neuron_hw.py
@@ -1,7 +1,7 @@
 from neuron import h
 from neuron import gui
 import matplotlib.pyplot as plt
-from pylab import array
+import numpy as np
 
 # Soma createion
 soma = h.Section(name = 'soma')
@@ -35,12 +35,12 @@ dend_vec = h.Vector()
 
 soma_vec.record(soma(0.5)._ref_v)
 time_vec.record(h._ref_t)
-dend_vec.record(dend(0.5)._ref_v) #??
+dend_vec.record(dend(0.5)._ref_v)
 h.tstop = 40
 h.run()
-time_vec = array(time_vec)
-soma_vec = array(soma_vec)
-dend_vec = array(dend_vec)
+time_vec = np.array(time_vec)
+soma_vec = np.array(soma_vec)
+dend_vec = np.array(dend_vec)
 time_vec = time_vec[10:]
 soma_vec = soma_vec[10:]
 dend_vec = dend_vec[10:]
@@ -49,4 +49,3 @@ plt.plot(time_vec, soma_vec, time_vec, dend_vec)
 plt.legend(['soma', 'dend'])
 plt.grid(True)
 plt.show()
-input()
