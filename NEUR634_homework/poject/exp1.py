3966256d52a2b25e0dc86c58210dfd9b0affe80c
sriram161 <skappag@masonlive.gmu.edu>
2018-11-29 03:57:33 -0500

    Chirp coded and test with moose components.

--- NEUR634_homework/poject/exp1.py
+++ NEUR634_homework/poject/exp1.py
@@ -4,6 +4,7 @@
 #bash-4.2$ module add moose
 
 import moose
+import numpy as np
 import matplotlib.pyplot as plt
 from utilities import create_comp_model
 from utilities import create_pulse_generator
@@ -24,6 +25,20 @@ VDIVS = 3000
 CAMAX = 1
 CAMIN = 0
 CADIVS = 10E3
+
+def chirp(gen_name="chirp", f0=1, f1=50, T=0.8, start=0.1, end=0.5, simdt=10E-5):
+    func_1 = moose.Func("/"+gen_name)
+    func_1.mode = 3
+    func_1.expr = 'cos(2*pi*({f1}-{f0})/{T}*x^2 + 2*pi*{f1}*x)'.format(f0=f0, f1=f1, T=T)
+    input = moose.StimulusTable('/xtab')
+    xarr = np.arange(start, end, simdt)
+    input.vector = xarr
+    input.startTime = 0.0
+    input.stepPosition = xarr[0]
+    input.stopTime = xarr[-1] - xarr[0]
+    moose.connect(input, 'output', func_1, 'xIn')
+    return func_1
+
 
 def cell_prototype(model_name, file_name, comp_passive, channel_settings, caparams=None):
     model = create_comp_model(model_name, file_name, comp_RM=comp_passive['RM'],
@@ -54,7 +69,7 @@ def cell_prototype(model_name, file_name, comp_passive, channel_settings, capara
 
 def main(model_name, file_name, comp_passive, channel_settings):
     # Simulation information.
-    simtime = 900
+    simtime = 50E-3
     simdt = 0.25e-5
     plotdt = 0.25e-3
 
@@ -63,6 +78,10 @@ def main(model_name, file_name, comp_passive, channel_settings):
     soma = moose.element(cell.path +'/soma')
 
     pulse_inject_50pA = create_pulse_generator('pulse2', soma, 50E-3, 50E-12, delay=10E-3)
+    chirp_test = chirp(gen_name="chirp", f0=1, f1=50, T=0.8, start=10E-3, end=20E-3, simdt=simdt)
+    tab = moose.Table('/check')
+    moose.connect(tab, 'requestOut', chirp_test, 'getValue')
+    moose.connect(chirp_test, 'valueOut', soma, 'injectMsg')
 
     # Output table
     soma_v_table = create_output_table(table_element='/output', table_name='somaVm')
